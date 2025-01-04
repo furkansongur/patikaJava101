40e28f87f7b6155cff2292ab11d8a1adcb150bca
Furkan Songur <69865431+furkansongur@users.noreply.github.com>
2025-01-04 17:40:19 +0300

    Add files via upload

--- src/GradePointAverage.java
+++ src/GradePointAverage.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class GradePointAverage {
     public static void main(String[] args) {
         int mat, fiz, kim, tur, tar, muz;
-        double average;
+        double average = 0;
 
         Scanner input = new Scanner(System.in);
 
@@ -13,19 +13,26 @@ public class GradePointAverage {
         System.out.print("Fizik Notunuz: ");
         fiz = input.nextInt();
 
-        System.out.println("Kimya Notunuz: ");
+        System.out.print("Kimya Notunuz: ");
         kim = input.nextInt();
 
-        System.out.println("Türkçe Notunuz: ");
+        System.out.print("Türkçe Notunuz: ");
         tur = input.nextInt();
 
-        System.out.println("Tarih Notunuz: ");
+        System.out.print("Tarih Notunuz: ");
         tar = input.nextInt();
 
-        System.out.println("Müzik Notunuz: ");
+        System.out.print("Müzik Notunuz: ");
         muz = input.nextInt();
-
-        average = (mat + fiz + kim + tur + tar + muz) / 6.0;
+        
+        
+        if (mat < 0 || mat > 100 || fiz < 0 || fiz > 100 || kim < 0 || kim > 100 || tur < 0 || tur > 100 || tar < 0 || tar > 100 || muz < 0 || muz > 100) {
+            System.out.println("Geçersiz not girdiniz!");
+            System.exit(0);
+        } else {
+            average = (mat + fiz + kim + tur + tar + muz) / 6.0;
+        }
+        
 
         if (average > 60) {
             System.out.println("Sınıfı Geçtiniz!");
